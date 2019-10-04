b357d20f127c28614d65e8563f2c373804a90abb
Evan Cleary <evanjcleary@gmail.com>
2019-10-04 00:29:42 -0400

    Updates

---
+++ precache-manifest.a4e444540ad7e81f0f52d3e721c642af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20ca9a771e249ddeda4886797ad1e09",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "bbce981408c5fc01a7ef",
+    "url": "/MemoryGame/static/css/main.a3922ca0.chunk.css"
+  },
+  {
+    "revision": "8badb62a122794a1cb02",
+    "url": "/MemoryGame/static/js/2.169990ca.chunk.js"
+  },
+  {
+    "revision": "bbce981408c5fc01a7ef",
+    "url": "/MemoryGame/static/js/main.84785426.chunk.js"
+  },
+  {
+    "revision": "6f01aab4ae7b4a83a806",
+    "url": "/MemoryGame/static/js/runtime-main.9b821b8e.js"
+  }
+]);
